99fe88174fbcf894bd4691186998935602f7f379
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-19 09:29:57 +0700

    update feauture

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
-let english_data = ["apple","banana","hello","world"];
-let vietnamese_data = ["tao","chuoi","xin chao","the gioi"];
-let index;
+let english_data = ["apple", "banana", "hello", "world"];
+let vietnamese_data = ["tao", "chuoi", "xin chao", "the gioi"];
+let index = null;
 let result = document.getElementById('result');
 let keyword;
 
-function translate_word(){
+function translate_word() {
     keyword = document.getElementById('keyword').value;
-    for (let i = 0 ; i < english_data.length ; i++){
-        if (keyword === english_data[i]){
+    for (let i = 0; i < english_data.length; i++) {
+        if (keyword === english_data[i]) {
             index = i;
             break;
         }
     }
-    result.innerHTML = "Meaning: " + vietnamese_data[index];
+    if (index === null) {
+        result.innerHTML = "No Match Case";
+    } else {
+        result.innerHTML = "Meaning: " + vietnamese_data[index];
+    }
 }
